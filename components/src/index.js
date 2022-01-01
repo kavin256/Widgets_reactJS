d0bab6d5e59ac6e2f3de60fddc7ac4cb68f73fdc
kavinranawella <kavin256@gmail.com>
2022-01-01 14:35:32 +0530

    components: nested components passing props and showing props.children

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from "./CommentDetail"; // as long as you are importing a js file. you don't have to mention the .js extension
+import CommentDetail from "./CommentDetail";
+import faker from "faker"; // as long as you are importing a js file. you don't have to mention the .js extension
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <ApprovalCard>
+                <div>
+                    <h4>
+                        Warning
+                    </h4>
+                </div>
+                Are you sure you wanna do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today @ 3:00AM" content="Are you sure?" url={faker.image.image()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today @ 2:00PM" content="Are you sure?"
+                               url={faker.image.image()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Today @ 5:00AM" content="Are you sure?"
+                               url={faker.image.image()}/>
+            </ApprovalCard>
         </div>
     );
 }
