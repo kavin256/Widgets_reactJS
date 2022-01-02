17de25544431014faccbacd18c785f38e7fef184
kavinranawella <kavin256@gmail.com>
2022-01-02 21:22:57 +0530

    components: adding spinner and helper render method

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,38 @@
+import './SeasonDisplay.css';
 import React from "react";
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: `Let's hit the frikin beach!`,
+        iconName: 'sun'
+    },
+    winter: {
+        text: `Burr.. it is chilly!`,
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season] // ES2015 syntax
+
     return (
-        <div>Hi there</div>
+        // here as you can see, the parent class name is the same as the component name. It's a good practice
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon icon-left massive`}/>
+            <h1>
+                {text}
+            </h1>
+            <i className={`${iconName} icon icon-right massive`}/>
+        </div>
     );
 }
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,28 +1,16 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-
-// import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    //     // THIS IS THE ONLY PLACE WE DO DIRECT ASSIGNMENT OF STATE
-    //     this.state = {lat: null, errorMessage: ''};
-    // }
 
-    // THIS IS THE ONLY PLACE WE DO DIRECT ASSIGNMENT OF STATE
     state = {lat: null, errorMessage: ''};
 
     componentDidMount() {
-        console.log('My component was rendered to the screen');
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                // setState updates the state. This is the only way of updating the state
                 this.setState({lat: position.coords.latitude})
-
-                // never do the following
-                // this.state.lat = position.coords.latitude;
             },
             (err) => {
                 this.setState({errorMessage: err.message})
@@ -30,20 +18,22 @@ class App extends React.Component {
         );
     }
 
-    componentDidUpdate() {
-        console.log('My component was just updated. It re-rendered')
-    }
-
-    // we have to define render method
-    render() {
-        console.log('My component Render........!')
-
+    // we can use normal function def or arrow def
+    renderContent = () => {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         } else if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>Loading..</div>
+        return <Spinner text='Please accept location request..'/>
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
