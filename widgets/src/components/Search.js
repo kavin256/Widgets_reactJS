f0ccb2fe4a9052854b9670d49149d6feaf8973e3
kavinranawella <kavin256@gmail.com>
2022-01-06 23:11:57 +0530

    widgets: adding search functionality with a delay

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,16 +20,35 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search().then(r => {
-    });
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
         <div key={result.pageid} className="item">
+          <div className="right floated content">
+            <a
+                className="ui button"
+                href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            >
+              Go
+            </a>
+          </div>
           <div className="content">
             <div className="header">{result.title}</div>
-            {result.snippet}
+            <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
           </div>
         </div>
     );
