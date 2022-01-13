4b982a29d2f2937f6da151f6073a09d79eb0837c
kavinranawella <kavin256@gmail.com>
2022-01-13 07:43:49 +0530

    widgets: routing

---
+++ widgets/src/components/Link.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Link = ({className, href, children}) => {
+  const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
+    event.preventDefault();
+    window.history.pushState({}, '', href); // rewriting URL
+
+    // communicate that URL has just changed
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+
+  return (
+      <a onClick={onClick} className={className} href={href}>
+        {children}
+      </a>
+  );
+};
+
+export default Link;
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
@@ -18,7 +18,7 @@ const Search = () => {
 
   useEffect(() => {
     const search = async () => {
-      const {data} = await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -37,37 +37,37 @@ const Search = () => {
 
   const renderedResults = results.map((result) => {
     return (
-        <div key={result.pageid} className="item">
-          <div className="right floated content">
-            <a
-                className="ui button"
-                href={`https://en.wikipedia.org?curid=${result.pageid}`}
-            >
-              Go
-            </a>
-          </div>
-          <div className="content">
-            <div className="header">{result.title}</div>
-            <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
-          </div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
     );
   });
 
   return (
-      <div>
-        <div className="ui form">
-          <div className="field">
-            <label>Enter Search Term</label>
-            <input
-                value={term}
-                onChange={(e) => setTerm(e.target.value)}
-                className="input"
-            />
-          </div>
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
-        <div className="ui celled list">{renderedResults}</div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
   );
 };
 
