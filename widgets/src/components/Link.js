22cf8b56f2141e53e772bca12884a190f2507211
kavinranawella <kavin256@gmail.com>
2022-01-13 07:50:18 +0530

    widgets: routing with cmd+click

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Link = ({className, href, children}) => {
   const onClick = (event) => {
+    // in the event of cmd+click, it should do the normal thing instead of the following lines
     if (event.metaKey || event.ctrlKey) {
       return;
     }
