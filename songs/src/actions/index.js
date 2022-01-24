5a4b94358b875c5c90988c40d530d0b6f92f621a
kavinranawella <kavin256@gmail.com>
2022-01-24 06:38:44 +0530

    song list: redux: adding action creator and calling connect method

---
+++ songs/src/actions/index.js
@@ -0,0 +1,9 @@
+// Action creator
+// named export
+export const selectSong = song => {
+    // return an action
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+}
---
+++ songs/src/components/SongList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {connect} from "react-redux";
+import {selectSong} from "../actions";
+
+class SongList extends React.Component {
+
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button
+                            onClick={() => this.props.selectSong(song)}
+                            className="ui button primary"
+                        >
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
+    render () {
+        // this.props === { songs: state.songs}
+        // console.log(this.props)
+        return <div className="ui divided list">{this.renderList()}</div>
+    }
+}
+
+// by convention this is called this name
+const mapStateToProps = (state) => {
+    // console.log(state)
+    return {songs: state.songs};
+}
+// connect does the calling of dispatch
+export default connect(mapStateToProps, {selectSong}) (SongList);
