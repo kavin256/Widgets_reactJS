961c41ffcaec639832f298fb9fcce013af399c76
kavinranawella <kavin256@gmail.com>
2022-01-24 06:41:16 +0530

    song list: test commit

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -24,7 +24,7 @@ class SongList extends React.Component {
 
     render () {
         // this.props === { songs: state.songs}
-        // console.log(this.props)
+        console.log(this.props)
         return <div className="ui divided list">{this.renderList()}</div>
     }
 }
